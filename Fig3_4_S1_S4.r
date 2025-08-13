88ced8e8f63ddd0c2c54643ae50524cd9df4b204
claudiamoreau <claudiamoreau1@gmail.com>
2025-08-13 11:07:22 -0400

    Update and rename Fig1_3_4_S1_S5.r to Fig3_4_S1_S4.r

--- Fig3_4_S1_S4.r
+++ Fig3_4_S1_S4.r
@@ -1,10 +1,10 @@
 ################################
 #
-# September 2024 by ?lisa Michel
-#
-# Modified by Claudia Moreau 2025
+# September 2024 by ME
 #
 # Do Figures in article
+#
+# Modified by CM
 ################################
 
 ### Packages
@@ -13,16 +13,10 @@ library('ggplot2')
 library("ggpubr")
 library("gridExtra")
 library("ggpmisc")
-#library('ggstats')
 library("stringr")
 library("stringi")
 library("tidyr")
 library(grid)
-#library("openxlsx")
-#library('plotly')
-library(htmlwidgets)
-library(patchwork)
-library(cowplot)
 
 
 
@@ -31,7 +25,6 @@ options(scipen=999)
 df_final <- read.table("comparison_WGS_Imput_Gnomad_all_variants_enriched__all_info_SAG_RQ_WQ.txt",header=T)
 ## Open file of variants known
 variants_known <- read.table("variants_reported_in_SLSJ.txt")
-#variants_new_whose_gene_reported <- read.table("/lustre03/project/6033529/saguenay_disease/Analyses_final_version_really/Article/variants_new_with_genes_reported_in_SLSJ.txt")
 
 ################################################################# Fig.3 ############################################################
 ######### Founder variants in SLSJ or UQc, graphs with CR UQc vs CR SLSJ
@@ -98,133 +91,19 @@ founder_WQ_known <- ggplot(data = df_for_graphs_RQ_SAG, aes(x = CR_SAG, y = CR_R
 					   labels = c(0,"1/2,000","1/4,000","1/6,000","1/8,000"))
 
 ## Save tiff format
-ggsave("Figure_3.tiff",plot = founder_WQ_known, width = 132, height = 132,dpi=600,units = "mm")
 tiff("Figure_3.tiff",width =132, height = 132,units = 'mm', res=600 ,compression = "lzw")
 founder_WQ_known
 dev.off()
 
-################################################################# Fig.1 ############################################################
-######### Graph Gnomad/(Imput if missing WGS) MAF on enriched Variants (1200) in QcP
-## Variants with freq of Gnomad
-NTNFE_WGS <- read.table("final_table_VTA_variant_enriched_in_WQ_compared_NTNFE_WGS_CaG_all_chr_tresh_0.1.txt", header =T)
-NTNFE_WGS$SNP <- paste0("chr",NTNFE_WGS$CHROM,"_",NTNFE_WGS$POS,"_",NTNFE_WGS$REF,"_",NTNFE_WGS$ALT)
-NTNFE_WGS <- NTNFE_WGS[,c(5,18,13,15)]
-NTNFE_imput <- read.table("final_table_VTA_variant_enriched_in_WQ_compared_NTNFE_Imput_CaG_tresh_0.1.txt", header =T)
-NTNFE_imput <- NTNFE_imput[,c(1,13,15)]
-## Create table
-mat = matrix(ncol = 0, nrow = 1304)
-df_gnomad=data.frame(mat)
-df_gnomad$SNP <- df_final$SNP
-df_gnomad <- merge(df_gnomad,NTNFE_imput,by='SNP', all.x=T, all.y=F)
-df_gnomad <- merge(df_gnomad,NTNFE_WGS,by='SNP', all.x=T, all.y=F)
-df_gnomad <- df_gnomad[,-2]
-names(df_gnomad) <- c("SNP","MAF_WQ_Imput","ID","MAF_GNOMAD","MAF_WQ_WGS")
-## Get MAF missing
-df_gnomad$MAF_WQ <- ifelse(is.na(df_gnomad$MAF_WQ_Imput),df_gnomad$MAF_WQ_WGS,df_gnomad$MAF_WQ_Imput)
 
-## Variants founder in QcP WGS and Imput
-variants_WGS_WQ_founder <- read.table('WGS/Table_founder_variants_in_WQ.txt',header=T)
-variants_Imput_WQ_founder <- read.table('Imput/Table_founder_variants_in_WQ.txt',header=T)
-
-## Get the status of the variant
-df_gnomad$founder_Imput_WQ <- df_final$founder_Imput_WQ
-df_gnomad$founder_WGS_WQ <- df_final$founder_WGS_WQ
-
-## Get founder of either Imput and if missing WGS
-df_gnomad$Founder_status<-  ifelse(is.na(df_gnomad$founder_Imput_WQ),df_gnomad$founder_WGS_WQ,df_gnomad$founder_Imput_WQ )
-## Replace not_founder by Not founder
-df_gnomad$Founder_status<-  gsub("Not_founder", "Non founder with relative difference ≥ 0.1", df_gnomad$Founder_status)
-df_gnomad$Founder_status<-  ifelse(is.na(df_gnomad$Founder_status),"Non founder with relative difference ≥ 0.1",df_gnomad$Founder_status)
-## Get if known or new
-df_gnomad$Founder_status<-  ifelse((df_gnomad$ID %in% variants_Imput_WQ_founder$ID | (!(df_gnomad$ID %in%variants_Imput_WQ_founder) & df_gnomad$ID %in% variants_WGS_WQ_founder$ID)),
-								   "New founder variant",df_gnomad$Founder_status)
-df_gnomad$Founder_status<-  ifelse((df_gnomad$ID %in% variants_known[,1] & df_gnomad$Founder_status == "New founder variant"),
-								   "Known founder variant",df_gnomad$Founder_status)
-
-## Do figure
-gnomad_vs_founder_enriched_only <- ggplot(data = df_gnomad,aes(x = MAF_WQ, y = MAF_GNOMAD)) +
-	geom_point(size=0.1)+
-	geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "grey") +
-	scale_shape_manual(values=c(1,5))+
-	theme(axis.text.x = element_text( size = 12),axis.title=element_text(size=12),
-		  axis.text.y = element_text(size = 12),
-		  plot.margin = unit(c(0.8, 0.2, 0, 0.2), "cm")) +
-	xlab("Frequency in the QcP") + ylab("Frequency in gnomAD nfe")  +xlim(0,0.03)+ylim(0,0.02) 
-
-
-######### Graph Gnomad/(Imput if missing WGS) MAF on enriched Variants (1304) in SLSJ 
-## Variants with freq of Gnomad
-## Get MAF for SLSJ in Imput ad WGS
-NTNFE_WGS_SAG <- read.table("SLSJ/mymerged_all_chr_CaG_seq_VTA_SAG_enriched.frq", header =T)
-NTNFE_WGS_SAG <- NTNFE_WGS_SAG[,c(2,5)]
-NTNFE_imput_SAG <- read.table("SLSJ/mymerged_all_chr_Imput_VTA_SAG_enriched.frq", header =T)
-NTNFE_imput_SAG <- NTNFE_imput_SAG[,c(2,5)]
-NTNFE_imput_SAG$SNP <- gsub(":", "_", NTNFE_imput_SAG$SNP)
-## Get freq in gnomad
-Gnomad_freq <- read.table("final_table_VTA_variant_enriched_in_WQ_compared_NTNFE_WGS_CaG_all_chr_tresh_0.1.txt", header =T)
-Gnomad_freq$SNP <- paste0("chr",Gnomad_freq$CHROM,"_",Gnomad_freq$POS,"_",Gnomad_freq$REF,"_",Gnomad_freq$ALT)
-Gnomad_freq <- Gnomad_freq[,c(5,13,18)]
-
-## Create table
-df_gnomad_SAG=data.frame(mat)
-df_gnomad_SAG$SNP <- df_final$SNP
-df_gnomad_SAG <- merge(df_gnomad_SAG,NTNFE_imput_SAG,by='SNP', all.x=T, all.y=F)
-df_gnomad_SAG <- merge(df_gnomad_SAG,NTNFE_WGS_SAG,by='SNP', all.x=T, all.y=F)
-df_gnomad_SAG <- merge(df_gnomad_SAG,Gnomad_freq,by='SNP', all.x=T, all.y=F)
-names(df_gnomad_SAG) <- c("SNP","MAF_SAG_Imput","MAF_SAG_WGS","ID","MAF_GNOMAD")
-
-## Select founder variants in SLSJ (Imput and WGS if missing)
-## Variants founder in SLSJ WGS and Imput
-variants_WGS_SAG_founder <- read.table('WGS/Table_founder_variants_in_SLSJ.txt',header=T)
-variants_Imput_SAG_founder <- read.table('Imput/Table_founder_variants_in_SLSJ.txt',header=T)
-
-## Get the status of the variant
-df_gnomad_SAG$founder_Imput_SAG <- df_final$founder_Imput_SAG
-df_gnomad_SAG$founder_WGS_SAG <- df_final$founder_WGS_SAG
-
-## Get founder of either Imput and if missing WGS
-df_gnomad_SAG$Founder_status<-  ifelse(is.na(df_gnomad_SAG$founder_Imput_SAG),df_gnomad_SAG$founder_WGS_SAG,df_gnomad_SAG$founder_Imput_SAG)
-## Replace not_founder by Not founder
-df_gnomad_SAG$Founder_status<-  gsub("Not_founder", "Non founder with relative difference ≥ 0.1", df_gnomad_SAG$Founder_status)
-df_gnomad_SAG$Founder_status<-  ifelse(is.na(df_gnomad_SAG$Founder_status),"Non founder with relative difference ≥ 0.1",df_gnomad_SAG$Founder_status)
-## Get if known or new
-df_gnomad_SAG$Founder_status<-  ifelse((df_gnomad_SAG$ID %in% variants_Imput_SAG_founder$ID | (!(df_gnomad_SAG$ID %in%variants_Imput_SAG_founder) & df_gnomad_SAG$ID %in% variants_WGS_SAG_founder$ID)),
-									   "New founder variant",df_gnomad_SAG$Founder_status)
-df_gnomad_SAG$Founder_status<-  ifelse((df_gnomad_SAG$ID %in% variants_known[,1]& df_gnomad_SAG$Founder_status == "New founder variant"), 
-									   "Known founder variant",df_gnomad_SAG$Founder_status)
-
-## Get MAF (Imput or WGS if missing)
-df_gnomad_SAG$MAF_SAG <- ifelse(is.na(df_gnomad_SAG$MAF_SAG_Imput),df_gnomad_SAG$MAF_SAG_WGS,df_gnomad_SAG$MAF_SAG_Imput)
-
-
-## Do figure
-gnomad_vs_founder_SAG_enriched_only <- ggplot(data = df_gnomad_SAG,aes(x = MAF_SAG, y = MAF_GNOMAD)) + 
-	geom_point(size=0.1)+
-	geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "grey") +
-	scale_shape_manual(values=c(1,5))+
-	theme(axis.text.x = element_text(size = 12),axis.title=element_text(size=12),
-		  axis.text.y = element_blank(),
-		  plot.margin = unit(c(0.8, 0.2, 0, 0.2), "cm")) +
-	theme(legend.title = element_blank())+ guides(color=guide_legend(override.aes = list(size=3)))+
-	rremove("ylab") +
-	# scale_x_log10() + scale_y_log10() +
-	xlab("Frequency in SLSJ") + ylab("Frequency in gnomAD nfe")  +xlim(0,0.03)+ylim(0,0.02) 
-
-
-##### -- FIRST FIGURE --
-figure_1_freq_Gnomad <- ggarrange(gnomad_vs_founder_enriched_only, gnomad_vs_founder_SAG_enriched_only, ncol=2, common.legend = TRUE, legend="bottom",labels = c("A", "B"),label.y = 1,widths = c(1.2,1))## Save tiff format
-ggsave("Figure_1.tiff",plot=figure_1_freq_Gnomad, width = 132, height = 100,dpi=600,units = "mm", bg="white")
-
-tiff("Figure_1.tiff",width =132, height = 132,units = 'mm', res=600 ,compression = "lzw")
-figure_1_freq_Gnomad
-dev.off()
 
 ################################################################# Fig.4 ############################################################
+##### Figure Simon with CR > 1/1000 for SLSJ
 ## Select row I want
 
 CR_for_graph <- df_final[,c(1,2,6,9,14,17,22,25,18,19,10,11,26,27)]
 ## Remove variants to freq
-variants_to_freq <- c("chr6_26090951_C_G","chr5_126577145_T_C","chr14_94380925_T_A","chr4_102901325_AAC_A")
+variants_to_freq <- c("chr6_26090951_C_G","chr14_94380925_T_A","chr4_102901325_AAC_A")
 CR_for_graph <- CR_for_graph[!CR_for_graph$SNP %in% variants_to_freq,]
 ## Select founder in SLSJ and UQc
 CR_for_graph$Founder_SAG[!is.na(CR_for_graph$CR_Imput_SAG)] <- CR_for_graph$founder_Imput_SAG[!is.na(CR_for_graph$CR_Imput_SAG)]
@@ -234,14 +113,15 @@ CR_for_graph$Founder_WQ[!is.na(CR_for_graph$CR_Imput_WQ)] <- CR_for_graph$founde
 CR_for_graph <- CR_for_graph[CR_for_graph$Founder_SAG == "Founder" | CR_for_graph$Founder_RQ == "Founder"| CR_for_graph$Founder_WQ == "Founder",]
 CR_for_graph <- CR_for_graph[!is.na(CR_for_graph$SNP),]
 
+
 ## Only take imput
 CR_for_graph$CR_SAG <- CR_for_graph$CR_Imput_SAG[!is.na(CR_for_graph$CR_Imput_SAG)]
 CR_for_graph$CR_RQ <- CR_for_graph$CR_Imput_RQ[!is.na(CR_for_graph$CR_Imput_RQ)]
 
+
 CR_for_graph[is.na(CR_for_graph)] <- -1
 
 # Create bins with intervals of 20 for UQc
-## Claudia modified 11 juin 2025 - until 1/1000
 bins_rq <- seq(0, 1000, by = 100)
 # Cut the RQ data into bins
 CR_for_graph$carrier_rate_intervals <- cut(CR_for_graph$CR_RQ, breaks = bins_rq, right = FALSE)
@@ -269,21 +149,21 @@ frequency_df_sag$dataset <- "SAG"
 
 # Combine the frequency dataframes for UQc and SLSJ
 combined_frequency <- rbind(frequency_df_rq, frequency_df_sag)
-
-
-
+sum(combined_frequency$Freq[combined_frequency$dataset=="SAG"])
 ########################## Enriched 
 ## Select row I want
 CR_for_graph_enriched <- df_final[,c(1,2,6,9,14,17,22,25,18,19,10,11)]
 ## Remove variants to freq
-variants_to_freq <- c("chr6_26090951_C_G","chr5_126577145_T_C","chr14_94380925_T_A","chr4_102901325_AAC_A")
+variants_to_freq <- c("chr6_26090951_C_G","chr14_94380925_T_A","chr4_102901325_AAC_A")
 CR_for_graph_enriched <- CR_for_graph_enriched[!CR_for_graph_enriched$SNP %in% variants_to_freq,]
 
-## Get CR ( imput only)
+## Get CR (either imput or if missing)
 CR_for_graph_enriched$CR_SAG[!is.na(CR_for_graph_enriched$CR_Imput_SAG)] <- CR_for_graph_enriched$CR_Imput_SAG[!is.na(CR_for_graph_enriched$CR_Imput_SAG)]
 CR_for_graph_enriched$CR_RQ[!is.na(CR_for_graph_enriched$CR_Imput_RQ)] <- CR_for_graph_enriched$CR_Imput_RQ[!is.na(CR_for_graph_enriched$CR_Imput_RQ)]
 ## Only take imput variants
 CR_for_graph_enriched <- CR_for_graph_enriched[!is.na(CR_for_graph_enriched$CR_SAG) | !is.na(CR_for_graph_enriched$CR_RQ),]
+
+
 CR_for_graph_enriched[is.na(CR_for_graph_enriched)] <- -1
 
 # Create bins with intervals of 20 for UQc
@@ -426,7 +306,7 @@ ggsave("Figure_supp_1.pdf", width = 107, height = 107,bg="white",dpi=300, units=
 
 
 
-################################################################# Supp Fig.5 ############################################################
+################################################################# Supp Fig.4 ############################################################
 ######### Graph Correlation Imput / WGS 
 ## Select data for Graph 
 df_final_hist <- df_final[,c(1:23)]
@@ -452,6 +332,10 @@ figure_method_compare_WGS_Imput <- ggplot(data = df_final_hist_CR_both_complete,
 	theme(legend.title = element_blank())+ guides(color=guide_legend(override.aes = list(size=4)))+
 	xlab("Variant frequency in WGS") + ylab("Variant frequency in imputed data")+
 	geom_text(x = 0.002, y = 0.015, label = lm_eqn(df_final_hist_CR_both_complete), parse = TRUE,size=6)
+
+## Save in tiff format
+ggsave("Figure_supp_4.tiff", width = 132, height = 132,units = 'mm',dpi=600)
+
 
 ## Save in tiff format
 ggsave("Figure_supp_5.tiff", width = 132, height = 132,units = 'mm',dpi=600)
